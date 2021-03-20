e145574c184347b2807edd40a3478c8039dbd49d
Artem <64410376+Artiss90@users.noreply.github.com>
2021-03-21 01:26:09 +0200

    Contactlist in hooks

--- src/Components/ContactList/ContactList.jsx
+++ src/Components/ContactList/ContactList.jsx
@@ -1,6 +1,5 @@
-import { connect } from 'react-redux';
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import { contactsOperations, contactsSelectors } from 'redux/contactsRedux';
 //* импорт стилей
 import style from './ContactList.module.css';
@@ -9,53 +8,31 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 /* eslint react/prop-types: 1 */
 
-class ContactList extends Component {
-  static propTypes = {
-    contacts: PropTypes.arrayOf(
-      PropTypes.exact({
-        id: PropTypes.string,
-        name: PropTypes.string,
-        number: PropTypes.string,
-      }),
-    ).isRequired,
-    onClickDelete: PropTypes.func.isRequired,
-  };
-  render() {
-    const { contacts, onClickDelete } = this.props;
-    return (
-      // TODO Добавление анимации на добавление-удаление списка контактов
-      <TransitionGroup component="ul" className={style.list}>
-        {contacts &&
-          contacts.map(contact => (
-            <CSSTransition key={contact.id} timeout={250} classNames={fade}>
-              <li key={contact.id} className={style.item}>
-                <p className={style.contact}>
-                  <span>{contact.name}:</span>
-                  <span>{contact.number}</span>
-                </p>
-                <button type="button" onClick={() => onClickDelete(contact.id)}>
-                  Delete
-                </button>
-              </li>
-            </CSSTransition>
-          ))}
-      </TransitionGroup>
-    );
-  }
+function ContactList() {
+  const dispatch = useDispatch();
+  // TODO фильтруем, показываем только те что совпадают*/
+  const contacts = useSelector(contactsSelectors.getVisibleFilterContacts);
+
+  const onClickDelete = id => dispatch(contactsOperations.deleteContact(id));
+  return (
+    // TODO Добавление анимации на добавление-удаление списка контактов
+    <TransitionGroup component="ul" className={style.list}>
+      {contacts &&
+        contacts.map(contact => (
+          <CSSTransition key={contact.id} timeout={250} classNames={fade}>
+            <li key={contact.id} className={style.item}>
+              <p className={style.contact}>
+                <span>{contact.name}:</span>
+                <span>{contact.number}</span>
+              </p>
+              <button type="button" onClick={() => onClickDelete(contact.id)}>
+                Delete
+              </button>
+            </li>
+          </CSSTransition>
+        ))}
+    </TransitionGroup>
+  );
 }
 
-const mapStateToProps = state => {
-  /** фильтруем, показываем только те что совпадают*/
-  return {
-    contacts: contactsSelectors.getVisibleFilterContacts(state),
-    isLoadingContact: contactsSelectors.getContactsLoading(state),
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onClickDelete: id => dispatch(contactsOperations.deleteContact(id)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(ContactList);
+export default ContactList;

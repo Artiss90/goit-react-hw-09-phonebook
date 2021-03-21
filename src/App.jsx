e00954617b4ece4abacd5e914f89ebc34cdb9102
Artem <64410376+Artiss90@users.noreply.github.com>
2021-03-21 02:53:26 +0200

    App on hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import AppBar from 'Components/AppBar';
-import React, { Component, lazy, Suspense } from 'react';
+import React, { lazy, memo, Suspense, useEffect } from 'react';
 import { Switch } from 'react-router';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { authOperations } from 'redux/auth';
 import PrivateRoute from 'Components/PrivateRoute';
 import PublicRoute from 'Components/PublicRoute';
@@ -23,44 +23,42 @@ const LazyLoginView = lazy(() =>
   import('./views/LoginView/LoginView' /* webpackChunkName "login-page"*/),
 );
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onGetCurrentUser();
-  }
+function App() {
+  const dispatch = useDispatch();
+  const onGetCurrentUser = () => dispatch(authOperations.fetchCurrentUser());
 
-  render() {
-    return (
-      <>
-        <AppBar />
-        <Suspense fallback={<p>Loading... Please waite</p>}>
-          <Switch>
-            <PublicRoute exact path="/" component={LazyHomeView} />
-            <PublicRoute
-              path="/register"
-              restricted
-              redirectTo="/contacts"
-              component={LazyRegisterView}
-            />
-            <PublicRoute
-              path="/login"
-              restricted
-              redirectTo="/contacts"
-              component={LazyLoginView}
-            />
-            <PrivateRoute
-              path="/contacts"
-              redirectTo="/login"
-              component={LazyContactsView}
-            />
-          </Switch>
-        </Suspense>
-      </>
-    );
-  }
+  useEffect(() => {
+    onGetCurrentUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <AppBar />
+      <Suspense fallback={<p>Loading... Please waite</p>}>
+        <Switch>
+          <PublicRoute exact path="/" component={LazyHomeView} />
+          <PublicRoute
+            path="/register"
+            restricted
+            redirectTo="/contacts"
+            component={LazyRegisterView}
+          />
+          <PublicRoute
+            path="/login"
+            restricted
+            redirectTo="/contacts"
+            component={LazyLoginView}
+          />
+          <PrivateRoute
+            path="/contacts"
+            redirectTo="/login"
+            component={LazyContactsView}
+          />
+        </Switch>
+      </Suspense>
+    </>
+  );
 }
 
-const mapDispatchToProps = {
-  onGetCurrentUser: authOperations.fetchCurrentUser,
-};
-
-export default connect(null, mapDispatchToProps)(App);
+export default memo(App);
